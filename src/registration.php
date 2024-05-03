a7385fc23e4df92ded2ea89b2b49a4823e67c811
Alastair Mucklow <alastair.mucklow@dotdigital.com>
2024-05-03 10:25:09 +0100

    Module boilerplate

---
+++ src/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Hyva_CheckoutDotdigitalgroupSms', __DIR__);
