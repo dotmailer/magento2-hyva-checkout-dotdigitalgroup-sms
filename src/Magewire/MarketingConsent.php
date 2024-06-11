b0fc5f938670a8e65dc0b78415391155ea69187b
Shaun Hogan <shaun.hogan@dotdigital.com>
2024-06-11 09:41:43 +0200

    Guest checkout marketing consent and shipping phone validation

--- src/Magewire/MarketingConsent.php
+++ src/Magewire/MarketingConsent.php
@@ -6,14 +6,21 @@ namespace Hyva\CheckoutDotdigitalgroupSms\Magewire;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magewirephp\Magewire\Component;
+use Dotdigitalgroup\Sms\ViewModel\TelephoneInputConfig;
+use Dotdigitalgroup\Sms\ViewModel\Customer\Account\MarketingConsent as MarketingConsentViewModel;
 
 class MarketingConsent extends Component
 {
 
-    public $isCheckboxChecked = false;
-    public $inputField = '';
+    /**
+     * @var MarketingConsentViewModel
+     */
+    private $marketingConsent;
 
-    public function __construct() {
+    public function __construct(
+        MarketingConsentViewModel $marketingConsent
+    ) {
+        $this->marketingConsent = $marketingConsent;
     }
 
     /**
@@ -22,7 +29,27 @@ class MarketingConsent extends Component
     public function boot(): void
     {
 
+        Parent::boot();
     }
 
+    public function getMarketingConsentLabel(): string
+    {
+        return $this->marketingConsent->getSmsSignUpText();
+    }
+
+    public function getMarketingConsentText(): string
+    {
+        return $this->marketingConsent->getSmsMarketingConsentText();
+    }
+
+    public function getStoredMobileNumber(): string
+    {
+        return $this->marketingConsent->getStoredMobileNumber();
+    }
+
+    public function getIsSubscribed(): bool
+    {
+        return $this->marketingConsent->isSubscribed() ?? false;
+    }
 
 }
